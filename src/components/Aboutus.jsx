ae2fbb77533d8e95b78bf99f633d0e632ec65c8a
7859950358 <mayurbhalu135@gmail.com>
2024-07-10 18:33:18 +0530

    add dark and light mode

---
+++ src/components/Aboutus.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Aboutus extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1 className='text-center'>Coming Soon</h1>
+        <h1 className='text-center'>Coming Soon</h1>
+        <h1 className='text-center'>Coming Soon</h1>
+      </div>
+    )
+  }
+}
